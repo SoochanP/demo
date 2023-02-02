341b2ac1416b6285c1869d05f31685d7962ea26c
SoochanP <121854793+SoochanP@users.noreply.github.com>
2023-02-02 12:29:17 +0900

    java 만듬!

--- demo/src/main/java/com/test/demo/Controller/BoardController.java
+++ demo/src/main/java/com/test/demo/Controller/BoardController.java
@@ -1,4 +1,4 @@
-package com.test.demo.board;
+package com.test.demo.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ demo/src/main/java/com/test/demo/Domain/BoardVO.java
@@ -0,0 +1,13 @@
+package com.test.demo.Domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardVO {
+
+	private int Date;
+}
