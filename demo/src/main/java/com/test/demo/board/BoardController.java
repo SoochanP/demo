3e88c9c8c8649200f23c495a389be9ae0ae22448
SoochanP <121854793+SoochanP@users.noreply.github.com>
2023-02-02 11:37:02 +0900

    first upload

---
+++ demo/src/main/java/com/test/demo/board/BoardController.java
@@ -0,0 +1,17 @@
+package com.test.demo.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+	@GetMapping("/")
+	public String main() {
+		return "index";
+	}
+	@GetMapping("board/list")
+	public String list() {
+		return "board/list";
+	}
+}
